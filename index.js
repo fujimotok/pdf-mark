0a1b5b6deb67bdc6c62cf1e428c0f72644085160
fujimotok <fujimotok@users.noreply.github.com>
2025-03-01 11:01:33 +0900

    refactoring.

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-let pdfDoc = null,
-    pageNum = 1,
-    pageRendering = false,
-    pageNumPending = null,
-    scale = 4.0,
-    canvas = document.createElement('canvas'),
-    ctx = canvas.getContext('2d');
+// -----------------------------------------------------------------------------
+// def var
+// -----------------------------------------------------------------------------
+let pdfDoc = null;
+let pageNum = 1;
+let pageRendering = false;
+let pageNumPending = null; // Page number during drawing
+let canvas = document.createElement('canvas');
 
+// -----------------------------------------------------------------------------
+// init element 
+// -----------------------------------------------------------------------------
 document.getElementById('pdf-viewer').appendChild(canvas);
 document.getElementById('page-num').textContent = 0;
 document.getElementById('page-count').textContent = 0;
 
-// SimpleMDEの設定
+const panZoom = panzoom(
+  document.getElementById('pdf-viewer'),
+  {
+    maxZoom: 10,
+    minZoom: 0.5,
+    initialZoom: 1,
+    initialX: 0,
+    initialY: 0,
+    bounds: true,
+    boundsPadding: 0.05
+  });
+
 const simplemde = new SimpleMDE({
   element: document.getElementById("markdown-editor"),
   autosave: {
@@ -23,35 +38,22 @@ const simplemde = new SimpleMDE({
   status: false
 });
 
-// ファイルが選択されたときの処理
-document.getElementById('file-input').addEventListener('change', function(event) {
-  const file = event.target.files[0];
-  if (file && file.type === 'application/pdf') {
-    const reader = new FileReader();
-    reader.onload = function(e) {
-      const uint8Array = new Uint8Array(e.target.result);
 
-      // PDF.jsを使ってPDFを表示
-      pdfjsLib.getDocument(uint8Array).promise.then(pdf => {
-        pdfDoc = pdf;
-        document.getElementById('page-count').textContent = pdfDoc.numPages;
-        renderPage(pageNum);
-      });
-    };
-    reader.readAsArrayBuffer(file);
-  }
-});
-
-// ページを表示する関数
+// -----------------------------------------------------------------------------
+// def func
+// -----------------------------------------------------------------------------
 function renderPage(num) {
   pageRendering = true;
   pdfDoc.getPage(num).then(function(page) {
+    const scale = 4.0;
     const viewport = page.getViewport({ scale: scale });
     canvas.height = viewport.height;
     canvas.width = viewport.width;
     canvas.style.height = (viewport.height / scale) + "px";
     canvas.style.width = (viewport.width / scale) + "px";
 
+    const ctx = canvas.getContext('2d');
+    
     const renderContext = {
       canvasContext: ctx,
       viewport: viewport
@@ -68,29 +70,29 @@ function renderPage(num) {
   });
 
   document.getElementById('page-num').textContent = num;
-
-  // And pass it to panzoom
-  const element = document.getElementById('pdf-viewer')
-  var instance = panzoom(element, {
-    maxZoom: 10, 		/* 拡大時の上限 */
-    minZoom: 0.5, 		/* 縮小時の下限 */
-    initialX: 0, 		/* コンテンツ表示の初期横位置 */
-    initialY: 0, 		/* コンテンツ表示の初期縦位置 */
-    initialZoom: 1, 	/* コンテンツ表示時の初期倍率 */
-    bounds: true, 		/* 表示領域外へ出ないようにする場合はtrue */
-    boundsPadding: 0.05 	/* bounds: true の時の表示余白 */
-  });
-
-  document.getElementById('reset').addEventListener('click', function(event) {
-    event.preventDefault();
-    simplemde.codemirror.focus();
-
-    instance.zoomAbs(0,0,1);
-    instance.moveTo(0, 0);
-  });
 }
 
-// ページ送りボタンのイベントリスナー
+
+// -----------------------------------------------------------------------------
+// add event
+// -----------------------------------------------------------------------------
+document.getElementById('file-input').addEventListener('change', function(event) {
+  const file = event.target.files[0];
+  if (file && file.type === 'application/pdf') {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      const uint8Array = new Uint8Array(e.target.result);
+
+      pdfjsLib.getDocument(uint8Array).promise.then(pdf => {
+        pdfDoc = pdf;
+        document.getElementById('page-count').textContent = pdfDoc.numPages;
+        renderPage(pageNum);
+      });
+    };
+    reader.readAsArrayBuffer(file);
+  }
+});
+
 document.getElementById('prev-page').addEventListener('click', function(event) {
   event.preventDefault();
   simplemde.codemirror.focus();
@@ -122,16 +124,23 @@ document.getElementById('menu').addEventListener('click', function() {
   }
 });
 
+document.getElementById('reset').addEventListener('click', function(event) {
+  event.preventDefault();
+  simplemde.codemirror.focus();
 
-// オーバーレイ要素の選択
-const overlayElement = document.getElementById('bottom-sheet');
-
-function eventHandler(event) {
-  event.stopPropagation();
-}
-
-// オーバーレイ要素でのイベントを無効にする
-const events = ['wheel', 'keydown', 'mousedown', 'dblclick', 'touchstart', 'touchmove', 'touchend']
-events.map(eventType => {
-  overlayElement.addEventListener(eventType, eventHandler);
+  panZoom.zoomAbs(0,0,1);
+  panZoom.moveTo(0, 0);
 });
+
+// Stop event propagation with overlay elements
+(function () {
+  const overlayElement = document.getElementById('bottom-sheet');
+  const events = ['wheel', 'keydown', 'mousedown', 'dblclick',
+                  'touchstart', 'touchmove', 'touchend']
+
+  events.map(eventType => {
+    overlayElement.addEventListener(eventType, function(event) {
+      event.stopPropagation();
+    });
+  });
+})();
