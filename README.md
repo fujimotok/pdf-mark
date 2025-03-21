b7306799e30fa31aadc9b0a39aec1506f5958143
fujimotok <fujimotok@users.noreply.github.com>
2025-03-21 12:05:30 +0900

    Changed to use PDF.js embedded viewer.

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 View PDF with Markdown Editor.
 
 # License
-[MIT license](https://en.wikipedia.org/wiki/MIT_License).
+- [MIT license](https://en.wikipedia.org/wiki/MIT_License).
+- pdfjs-5.0.375-dist : [Apache License 2.0](pdfjs-5.0.375-dist/LICENSE)
--- index.js
+++ index.js
@@ -1,31 +1,10 @@
 // -----------------------------------------------------------------------------
 // def var
 // -----------------------------------------------------------------------------
-let pdfDoc = null;
-let pageNum = 1;
-let pageRendering = false;
-let pageNumPending = null; // Page number during drawing
-let canvas = document.createElement('canvas');
 
 // -----------------------------------------------------------------------------
 // init element 
 // -----------------------------------------------------------------------------
-document.getElementById('pdf-viewer').appendChild(canvas);
-document.getElementById('page-num').value = 0;
-document.getElementById('page-count').textContent = 0;
-
-const panZoom = panzoom(
-  document.getElementById('pdf-viewer'),
-  {
-    maxZoom: 10,
-    minZoom: 0.5,
-    initialZoom: 1,
-    initialX: 0,
-    initialY: 0,
-    bounds: true,
-    boundsPadding: 0.05
-  });
-
 const simplemde = new SimpleMDE({
   element: document.getElementById("markdown-editor"),
   autosave: {
@@ -42,35 +21,6 @@ const simplemde = new SimpleMDE({
 // -----------------------------------------------------------------------------
 // def func
 // -----------------------------------------------------------------------------
-function renderPage(num) {
-  pageRendering = true;
-  pdfDoc.getPage(num).then(function(page) {
-    const scale = 4.0;
-    const viewport = page.getViewport({ scale: scale });
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
-    canvas.style.height = (viewport.height / scale) + "px";
-    canvas.style.width = (viewport.width / scale) + "px";
-
-    const ctx = canvas.getContext('2d');
-    
-    const renderContext = {
-      canvasContext: ctx,
-      viewport: viewport
-    };
-    const renderTask = page.render(renderContext);
-
-    renderTask.promise.then(function() {
-      pageRendering = false;
-      if (pageNumPending !== null) {
-        renderPage(pageNumPending);
-        pageNumPending = null;
-      }
-    });
-  });
-
-  document.getElementById('page-num').value = num;
-}
 
 
 // -----------------------------------------------------------------------------
@@ -79,48 +29,10 @@ function renderPage(num) {
 document.getElementById('file-input').addEventListener('change', function(event) {
   const file = event.target.files[0];
   if (file && file.type === 'application/pdf') {
-    const reader = new FileReader();
-    reader.onload = function(e) {
-      const uint8Array = new Uint8Array(e.target.result);
-
-      pdfjsLib.getDocument(uint8Array).promise.then(pdf => {
-        pdfDoc = pdf;
-        document.getElementById('page-count').textContent = pdfDoc.numPages;
-        document.getElementById('page-num').max = pdfDoc.numPages;
-        renderPage(pageNum);
-      });
-    };
-    reader.readAsArrayBuffer(file);
+    const fileUrl = URL.createObjectURL(file); // ローカルファイルのBlob URLを生成
+    const viewer = document.getElementById('pdf-viewer');
+    viewer.src = `pdfjs-5.0.375-dist/web/viewer.html?file=${encodeURIComponent(fileUrl)}`;
   }
-});
-
-document.getElementById('page-num').addEventListener('keydown', function(event) {
-  if (event.key === "Enter") {
-    pageNum = Number(document.getElementById('page-num').value)
-    renderPage(pageNum)
-  }
-});
-
-document.getElementById('prev-page').addEventListener('click', function(event) {
-  event.preventDefault();
-  simplemde.codemirror.focus();
-
-  if (pageNum <= 1) {
-    return;
-  }
-  pageNum--;
-  renderPage(pageNum);
-});
-
-document.getElementById('next-page').addEventListener('click', function(event) {
-  event.preventDefault();
-  simplemde.codemirror.focus();
-
-  if (pageNum >= pdfDoc.numPages) {
-    return;
-  }
-  pageNum++;
-  renderPage(pageNum);
 });
 
 document.getElementById('menu').addEventListener('click', function() {
@@ -130,14 +42,6 @@ document.getElementById('menu').addEventListener('click', function() {
   } else {
     element.classList.add('hidden');
   }
-});
-
-document.getElementById('reset').addEventListener('click', function(event) {
-  event.preventDefault();
-  simplemde.codemirror.focus();
-
-  panZoom.zoomAbs(0,0,1);
-  panZoom.moveTo(0, 0);
 });
 
 // Stop event propagation with overlay elements
